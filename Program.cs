363e28dbe0ab775ea695e4838c6ba5bddcec43a0
phillip deleon <phillipdeleon68@gmail.com>
2024-11-11 12:43:10 -0600

    update

--- Program.cs
+++ Program.cs
@@ -4,35 +4,7 @@ namespace Assignment_2_
 {
     internal class Program
     {
-        class Student
-        {
-            private int studentId; //field
-            private string studentFName;
-            private string studentLName;
-            private char studentGrade;
-
-            public int StudentId //property
-            {
-                get { return studentId; }
-                set { studentId = value; }
-            }
-            public string StudentFName 
-            {
-                get { return studentFName; }
-                set { studentFName = value; }
-            }
-            public string StudentLName 
-            {
-                get { return studentLName; }
-                set { studentLName = value; }
-            }
-            public char StudentGrade 
-            {
-                get { return studentGrade; }
-                set { studentGrade = value; }
-            }
-
-        }
+        
         static void Main(string[] args)
         {
             Student studentInfo = new Student(); //instantiate
@@ -45,13 +17,40 @@ namespace Assignment_2_
             studentInfo.StudentLName = Console.ReadLine();
             Console.WriteLine("Enter student's Grade: ");
             studentInfo.StudentGrade = Convert.ToChar(Console.ReadLine());
-
+            
             Console.WriteLine($"Student's ID: {studentInfo.StudentId}");
             Console.WriteLine($"Student's First Name: {studentInfo.StudentFName}");
             Console.WriteLine($"Student's Last Name: {studentInfo.StudentLName}");
             Console.WriteLine($"Student's Grade: {studentInfo.StudentGrade}");
+        } 
+    }
+    class Student
+    {
+        private int studentId; //field
+        private string studentFName;
+        private string studentLName;
+        private char studentGrade;
+
+        public int StudentId //property
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+        public string StudentFName
+        {
+            get { return studentFName; }
+            set { studentFName = value; }
+        }
+        public string StudentLName
+        {
+            get { return studentLName; }
+            set { studentLName = value; }
+        }
+        public char StudentGrade
+        {
+            get { return studentGrade; }
+            set { studentGrade = value; }
         }
 
-        
     }
 }
